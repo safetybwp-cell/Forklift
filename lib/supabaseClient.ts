afa52a9a4a9c78234fd45bdbcf578c3ff7ff38be
Kungjakkid <148857819+Kungjakkid@users.noreply.github.com>
2026-02-03 16:32:35 +0700

    Hardcode credentials for private repo deployment

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://caysnmdwyiqvavolkclw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNheXNubWR3eWlxdmF2b2xrY2x3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwOTc0OTgsImV4cCI6MjA4NTY3MzQ5OH0.ouUNLVGZ-h0tjaHQ3i430aMsFGl75yoDB0KrxRFws2s'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
